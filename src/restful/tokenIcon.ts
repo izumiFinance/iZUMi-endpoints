f7626be8ae971a25043ac3bc8187db1e48fdb958
AbcoldQAQ <lz841309023@gmail.com>
2024-12-30 17:35:26 +0800

    update token icon

---
+++ src/restful/tokenIcon.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export type BtcFunInfo = {
+    id: string;
+    token: string;
+    pool: string;
+    name: string;
+    symbol: string;
+    decimals: string;
+    image: string;
+};
+
+export type ResponseBtcFunInfo = {
+    data: {
+        data: BtcFunInfo;
+    };
+};
+
+export const getBtcFunInfo = async (symbol: string): Promise<ResponseBtcFunInfo> => {
+    return axios.get('https://api.btc.fun/api/v1/token/info?symbol=' + symbol);
+};
