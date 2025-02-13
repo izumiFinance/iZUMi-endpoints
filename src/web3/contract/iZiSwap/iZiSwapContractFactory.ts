fa0a649bb5a644ed851fd35fcf8470071d76c16b
LpcAries <291482574@qq.com>
2025-02-13 17:12:35 +0800

    update getPoolContractByAddress

--- src/web3/contract/iZiSwap/iZiSwapContractFactory.ts
+++ src/web3/contract/iZiSwap/iZiSwapContractFactory.ts
@@ -1,12 +1,11 @@
+import { Contract } from 'ethers';
 import izumiPoolABI from '../../abi/iZiSwap/Pool.json';
-import { PoolContract } from "../../types/iZiSwap/Pool";
-import Web3 from 'web3';
 import { getContract } from '../contractFactory';
 
-export const getPoolContractByAddress = (web3: Web3, contractAddress: string): PoolContract | undefined => {
-    if (!contractAddress || !web3) {
+export const getPoolContractByAddress = (provider: any, contractAddress: string): Contract | undefined => {
+    if (!contractAddress || !provider) {
         return undefined;
     }
 
-    return getContract<PoolContract>(izumiPoolABI, contractAddress, web3);
+    return getContract<Contract>(izumiPoolABI, contractAddress, provider);
 };
