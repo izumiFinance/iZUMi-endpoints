7aa1935af33e8a14dd5fc7fd13a0d7f9088cc089
LpcAries <291482574@qq.com>
2025-02-20 14:06:45 +0800

    update

--- src/web3/contract/veiZi/veiziContractFactory.ts
+++ src/web3/contract/veiZi/veiziContractFactory.ts
@@ -1,17 +1,16 @@
-import Web3 from "web3";
+import { Contract } from 'ethers';
 import veiziABI from '../../abi/veiZi/VeiZi.json';
-import { VeiZiContract } from "../../types/veiZi/VeiZi";
 import { getContract, getContractFromContext } from '../contractFactory';
 import { VEIZI_ADDRESS } from "./veiziContractConfig";
 
-export const getVeiZiContract = (chainId: number, web3: Web3): VeiZiContract | undefined => {
+export const getVeiZiContract = (chainId: number, provider: any): Contract | undefined => {
     const contractAddress = VEIZI_ADDRESS[chainId];
-    if (!contractAddress || !web3) {
+    if (!contractAddress || !provider) {
         return undefined;
     }
     // const contract = getContractFromContext(chainId, contractAddress);
     // if (contract) {
     //     return contract as VeiZiContract;
     // }
-    return getContract<VeiZiContract>(veiziABI, contractAddress, web3);
+    return getContract<Contract>(veiziABI, contractAddress, provider);
 };
